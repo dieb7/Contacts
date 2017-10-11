f7e549488d16f19145ed6142c40d75b074a56d6f
Diego Benavides <diegob41@gmail.com>
2017-10-10 21:13:29 -0600

    Added a very simple ListView with some hardcoded values. Also added a button to add contacts, but it does not do anything yet

--- app/src/main/java/com/example/diebm/contacts/ContactsActivity.java
+++ app/src/main/java/com/example/diebm/contacts/ContactsActivity.java
@@ -2,12 +2,36 @@ package com.example.diebm.contacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ContactsActivity extends AppCompatActivity {
+
+    private ListView my_contacts;
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        // Find the listView resource
+        my_contacts = (ListView) findViewById(R.id.contactsListView);
+
+        // Create and populate a list of user names.
+        String[] usernames = new String[] {"Fulanito", "Menganito", "Sutanito"};
+
+        ArrayList<String> usernameList = new ArrayList<String>();
+
+        usernameList.addAll(Arrays.asList(usernames));
+
+        // Create ArrayAdapter using the usenname list
+
+        listAdapter = new ArrayAdapter<String>(this, R.layout.stringrow, usernameList);
+
+        my_contacts.setAdapter(listAdapter);
     }
 }
