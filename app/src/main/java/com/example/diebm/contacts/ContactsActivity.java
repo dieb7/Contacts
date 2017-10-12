cda091824f8bc94afd598de4b707c52af92e48ce
Diego Benavides <diegob41@gmail.com>
2017-10-11 22:47:14 -0600

    Added an EditContactActivity, some more work is needed for this activity to work. No delete functionality yet and save is incomplete

--- app/src/main/java/com/example/diebm/contacts/ContactsActivity.java
+++ app/src/main/java/com/example/diebm/contacts/ContactsActivity.java
@@ -1,19 +1,25 @@
 package com.example.diebm.contacts;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.security.AccessControlContext;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static java.security.AccessController.getContext;
 
 public class ContactsActivity extends AppCompatActivity {
 
     private ListView my_contacts;
     private ArrayAdapter<User> listAdapter;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +30,35 @@ public class ContactsActivity extends AppCompatActivity {
         my_contacts = (ListView) findViewById(R.id.contactsListView);
 
         // Create and populate a list of users
-        UserList userList = new UserList();
+        final UserList userList = new UserList();
 
         userList.loadUsers(getApplicationContext());
 
         listAdapter = new UserAdapter(this, userList.getUsers());
 
         my_contacts.setAdapter(listAdapter);
+
+        context = this;
+
+        // When User is long clicked, this starts EditUserActivity
+        my_contacts.setOnItemLongClickListener(new android.widget.AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int pos, long id) {
+
+                User user = listAdapter.getItem(pos);
+
+                int meta_pos = userList.getIndex(user);
+
+                if (meta_pos >= 0) {
+
+                    Intent edit = new Intent(context, EditUserActivity.class);
+                    edit.putExtra("position", meta_pos);
+                    startActivity(edit);
+                }
+                return true;
+            }
+        });
     }
 
     public void addUserActivity(View view) {
